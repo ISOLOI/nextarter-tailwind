ac0d3bfe0c1cd0bddc2f6c544df3c2b6de0b6d8f
ISOLOI <nicholasjkaufmann@gmail.com>
2024-04-20 16:48:48 -0500

    initial

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+const NextSitemapConfig = {
+  siteUrl: "https://sage-kelpie-b015b6.netlify.app/",
+  generateRobotsTxt: true,
+};
+
+module.exports = NextSitemapConfig;
